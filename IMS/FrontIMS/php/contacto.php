c0a6198652cab972c1226bcbe8d4f9a143426f72
Miguel Gomez <miguelgcrespo07@gmail.com>
2019-03-28 14:45:48 -0600

    Pruebas de contacto

--- IMS/FrontIMS/php/contacto.php
+++ IMS/FrontIMS/php/contacto.php
@@ -1,3 +1,24 @@
-<php?
+<?php
+	$nombre = $_POST["nombre"];
+	$correo = $_POST["correo"];
+	$telefono = $_POST["telefono"];
+	$asunto = $_POST["asunto"];
+	$mensaje = $_POST["mensaje"];
+
+	$destinatario = "SoporteIMS@outlook.com";
+
+	$mensaje = "
+		Datos del remitente
+		Nombre: ".$nombre."
+		Correo: ".$correo."
+		Telefono: ".$telefono."
+
+		Mensaje/Comentario: ".$mensaje."
+	";
+	mail($destinatario,$asunto,utf8_decode($mensaje));
+	echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado exitosamente');</script>";
+	echo "<script type='text/javascript'>window.location.href='http://bluuweb.cl/plantilla-1/index.html';</script>";
+
+	header("location: ../Contacto.html");
 
 ?>
