fc879717e23ae63caedd98dad03f8760c1588334
Miguel Gomez <miguelgcrespo07@gmail.com>
2019-05-31 16:34:44 -0500

    Reportes

---
+++ admin/pdf/ventasm.php
@@ -0,0 +1,89 @@
+<?php
+require('fpdf/fpdf.php');
+
+class PDF extends FPDF
+{
+// Cabecera de página
+function Header()
+{
+
+    $this->Image('../img/LogoConNombre.png',10,5,40);
+
+    $this->Ln(10);
+    $this->Cell(65);
+    $this->SetFont('Arial','B',15);
+    $this->SetXY(70, 15);
+    $this->Cell(60,10,'Ventas mostrador',0,0,'C');
+    $this->Ln(20);
+}
+
+// Pie de página
+function Footer()
+{
+// 	Tel. 9 43 43 00
+// SoporteIMS@outlook.com
+// C. 28 x 19 y 17 519 
+// Col. Maya, Mérida, Yucatan
+    $this->SetY(-22);
+	$this->SetFont('Arial','',7);
+    $this->Cell(140);
+    $this->Cell(70,5,'Tel. 9 43 43 00',0,0,'C');
+    $this->ln();
+    $this->Cell(140);
+    $this->Cell(70,5,'SoporteIMS@outlook.com',0,0,'C');
+    $this->ln();
+    $this->Cell(140);
+    $this->Cell(70,5,'C. 28 x 19 y 17 519',0,0,'C');
+    $this->ln();
+    $this->Cell(140);
+    $this->Cell(70,5,utf8_decode('Col. Maya, Mérida, Yucatan'),0,0,'C');
+    $this->ln();
+
+    // Posición: a 1,5 cm del final
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Número de página
+    $this->Cell(0,10,'Pagina '.$this->PageNo().'/{nb}',0,0,'C');
+
+}
+}
+$fecha1 = $_POST ['finicio'];
+$fecha2 = $_POST ['ffin'];
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',8);
+//DATOS DE DETALLE DE VENTAS
+require('../php/conexion.php');
+
+$sql = "SELECT v.idVenta,v.FechaVenta,ci.NombreCli,ci.Apellido1Cli,ci.Apellido2Cli, ce.NombresEmp,ce.Apellido1Emp,ce.Apellido2Emp, v.totalVenta, cev.EstadoVenta FROM ventas v INNER JOIN cat_usuarios cu ON v.idCliente = cu.idUsuario INNER JOIN cat_clientes ci ON cu.idUsuario = ci.idUsuario INNER JOIN cat_empleados ce ON v.idEmpleado = ce.idEmpleado INNER JOIN cat_estadosventa cev ON v.idestadoVenta = cev.idEstadoVenta WHERE v.FechaVenta BETWEEN '$fecha1' AND '$fecha2' ORDER BY idVenta ASC";
+
+if(!$resultado = $conexion->query($sql)){
+    die('Ocurrio un error ejecutando el query [' . $conexion->error . ']');
+}
+
+
+
+$pdf->Cell(15,10,utf8_decode('Clave'),1,0,'C');
+$pdf->Cell(20,10,utf8_decode('Fecha'),1,0,'C');
+$pdf->Cell(55,10,utf8_decode('Cliente'),1,0,'C');
+$pdf->Cell(55,10,utf8_decode('Empleado'),1,0,'C');
+$pdf->Cell(20,10,utf8_decode('Importe'),1,0,'C');
+$pdf->Cell(25,10,utf8_decode('Etapa'),1,1,'C');
+
+$pdf->SetFont('Arial','',8);
+
+while($fila2 = $resultado->fetch_assoc()){
+    $pdf->Cell(15,10,utf8_decode($fila2['idVenta']),1,0,'C');
+    $pdf->Cell(20,10,utf8_decode($fila2['FechaVenta']),1,0,'C');
+    $pdf->Cell(55,10,utf8_decode($fila2['NombreCli']." ".$fila2['Apellido1Cli']." ".$fila2['Apellido2Cli']),1,0,'C');
+    $pdf->Cell(55,10,utf8_decode($fila2['NombresEmp']." ".$fila2['Apellido1Emp']." ".$fila2['Apellido2Emp']),1,0,'C');
+    $pdf->Cell(20,10,utf8_decode($fila2['totalVenta']),1,0,'C');
+    $pdf->Cell(25,10,utf8_decode($fila2['EstadoVenta']),1,1,'C');
+}
+
+
+
+$pdf->Output();
+?>
--- admin/php/reportes.php
+++ admin/php/reportes.php
@@ -9,6 +9,55 @@
 		}
 	}
 
+	if (isset($_POST['masvendido'])) {
+		$fecha1 = $_POST ['finicio'];
+		$fecha2 = $_POST ['ffin'];
+		include("conexion.php");
+
+		$sql = "SELECT * FROM ( SELECT SUM(dv.Cantidad) AS Cantidad,cp.NombreProd,cp.idProducto FROM detalle_venta dv INNER JOIN cat_productos cp ON dv.idProducto = cp.idProducto INNER JOIN ventas v ON dv.idVenta = v.idVenta WHERE v.FechaVenta BETWEEN '$fecha1' AND '$fecha2' GROUP BY dv.idProducto) C WHERE Cantidad = ( SELECT MAX(Cantidad) FROM ( SELECT SUM(dv.Cantidad) AS Cantidad,cp.NombreProd FROM detalle_venta dv INNER JOIN cat_productos cp ON dv.idProducto = cp.idProducto INNER JOIN ventas v ON dv.idVenta = v.idVenta WHERE v.FechaVenta BETWEEN '$fecha1' AND '$fecha2' GROUP BY dv.idProducto ) A )";
+		echo $sql;
+		if(!$resultado = $conexion->query($sql)){
+			die('Ocurrio un error ejecutando el query [' . $conexion->error . ']');
+		}
+				
+		$prodmasvend = $resultado->fetch_assoc();
+		mysqli_close($conexion);
+
+	}
+
+	if (isset($_POST['menosvendido'])) {
+		$fecha1 = $_POST ['finicio'];
+		$fecha2 = $_POST ['ffin'];
+		include("conexion.php");
+		$sql = "SELECT * FROM ( SELECT SUM(dv.Cantidad) AS Cantidad,cp.NombreProd,cp.idProducto FROM detalle_venta dv INNER JOIN cat_productos cp ON dv.idProducto = cp.idProducto INNER JOIN ventas v ON dv.idVenta = v.idVenta WHERE v.FechaVenta BETWEEN '$fecha1' AND '$fecha2' GROUP BY dv.idProducto) C WHERE Cantidad = ( SELECT MIN(Cantidad) FROM ( SELECT MAX(dv.Cantidad) AS Cantidad,cp.NombreProd FROM detalle_venta dv INNER JOIN cat_productos cp ON dv.idProducto = cp.idProducto INNER JOIN ventas v ON dv.idVenta = v.idVenta WHERE v.FechaVenta BETWEEN '$fecha1' AND '$fecha2' GROUP BY dv.idProducto ) A )";
+
+		if(!$resultado = $conexion->query($sql)){
+			die('Ocurrio un error ejecutando el query [' . $conexion->error . ']');
+		}
+		
+		$prodmenosvend = $resultado->fetch_assoc();
+		mysqli_close($conexion);
+
+	}
+
+	if (isset($_POST['mejorvendedor'])) {
+		$fecha1 = $_POST ['finicio'];
+		$fecha2 = $_POST ['ffin'];
+		include("conexion.php");
+		$sql = "SELECT COUNT(v.idVenta) AS Cantidad, ce.NombresEmp, ce.Apellido1Emp, ce.Apellido2Emp FROM ventas v INNER JOIN cat_empleados ce ON v.idEmpleado = ce.idEmpleado WHERE v.FechaVenta BETWEEN '$fecha1' AND '$fecha2' GROUP BY v.idEmpleado ORDER BY Cantidad DESC LIMIT 1";
+
+		if(!$resultado = $conexion->query($sql)){
+			die('Ocurrio un error ejecutando el query [' . $conexion->error . ']');
+		}
+		
+		$mejorvendedor = $resultado->fetch_assoc();
+		mysqli_close($conexion);
+
+	}
+
+
+
+
 
  ?>
  <!DOCTYPE html>
@@ -23,9 +72,106 @@
 	
 	<?php include ("../includes/menu.php") ?>
 	<section class="ContenedorPrincipal">
-	<h1>Necesita un respaldo de la base de datos?</h1>
-	<br><br>
-	<button id="btn-resp"><a href="crear_respaldo_bd.php">Crear respaldo</a></button>
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" class="formrep">
+			<label for="">Producto mas vendido</label>
+			<br><br><br>
+			<label for="">Fecha inicio </label><input class="fecha_rep" required type="date" name="finicio" value="<?php if (isset($fecha1)) echo $fecha1?>">
+			<label for="">Fecha limite </label><input class="fecha_rep" required type="date" name="ffin" value="<?php if (isset($fecha2)) echo $fecha2?>">
+			<br><br>
+			<button id="btn-resp" type="submit" name="masvendido">Buscar</button>
+			<br><br>
+			<?php if (isset($prodmasvend)) {
+			echo "<label for=''>
+			El producto mas vendido en este rango de fechas es: ".$prodmasvend['NombreProd']."
+			</label>";
+			
+			}?>
+			
+		</form>	
+		<hr>	
+		<hr>	
+
+		<br><br>
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" class="formrep">
+			<label for="">Producto menos vendido</label>
+			<br><br><br>
+			<label for="">Fecha inicio </label><input class="fecha_rep" required type="date" name="finicio" value="<?php if (isset($fecha1)) echo $fecha1?>">
+			<label for="">Fecha limite </label><input class="fecha_rep" required type="date" name="ffin" value="<?php if (isset($fecha2)) echo $fecha2?>">
+			<br>
+			<br>
+			<button id="btn-resp" type="submit" name="menosvendido">Buscar</button>
+			<br><br>
+			<?php if (isset($prodmenosvend)) {
+			echo "<label for=''>
+			El producto menos vendido en este rango de fechas es: ".$prodmenosvend['NombreProd']."
+			</label>";
+			
+			}?>
+			
+		</form>
+		<hr>	
+		<hr>	
+
+		<br><br>
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" class="formrep">
+			<label for="">Mejor vendedor</label>
+			<br><br><br>
+			<label for="">Fecha inicio </label><input class="fecha_rep" required type="date" name="finicio" value="<?php if (isset($fecha1)) echo $fecha1?>">
+			<label for="">Fecha limite </label><input class="fecha_rep" required type="date" name="ffin" value="<?php if (isset($fecha2)) echo $fecha2?>">
+			<br>
+			<br>
+			<button id="btn-resp" type="submit" name="mejorvendedor">Buscar</button>
+			<br><br>
+			<?php if (isset($mejorvendedor)) {
+			echo "<label for=''>
+			El vendedor que ha concretado mas ventas en este rango de fechas es: ".$mejorvendedor['NombresEmp']." ".$mejorvendedor['Apellido1Emp']." ".$mejorvendedor['Apellido2Emp']."
+			</label>";
+			}?>
+			
+		</form>
+			<hr>	
+		<hr>	
+		<br><br>
+		<form action="../pdf/ventasm.php" method="POST" class="formrep" target="_blank">
+			<label for="">Ventas mostrador</label>
+			<br><br><br>
+			<label for="">Fecha inicio </label><input class="fecha_rep" required type="date" name="finicio" value="<?php if (isset($fecha1)) echo $fecha1?>">
+			<label for="">Fecha limite </label><input class="fecha_rep" required type="date" name="ffin" value="<?php if (isset($fecha2)) echo $fecha2?>">
+			<br>
+			<br>
+				<button type="submit" id="btn-resp">
+						Buscar
+				</button>
+			</a>
+			<br><br>
+
+		</form>
+			<hr>	
+		<hr>	
+		<br><br>
+		<form action="../pdf/ventaslinea.php" method="POST" class="formrep" target="_blank">
+			<label for="">Ventas en linea</label>
+			<br><br><br>
+			<label for="">Fecha inicio </label><input class="fecha_rep" class="fecha_rep" required type="date" name="finicio" value="<?php if (isset($fecha1)) echo $fecha1?>">
+			<label for="">Fecha limite </label><input class="fecha_rep" class="fecha_rep" required type="date" name="ffin" value="<?php if (isset($fecha2)) echo $fecha2?>">
+			<br>
+			<br>
+				<button type="submit" id="btn-resp">
+						Buscar
+				</button>
+			</a>
+			
+		</form>
+		<br><br>
+		<form class="formrep" action="#">
+			<label for="">¿Necesita un respaldo de la base de datos?</label>
+			<button id="btn-resp"><a href="crear_respaldo_bd.php">Crear respaldo</a></button>
+			
+		</form>
+			<hr>	
+		<hr>	
+		<br><br>
+		
 	<br>
 	<br>
 	<br>
