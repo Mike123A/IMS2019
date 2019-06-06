f9102503d21467be684c9120b6c08ad23561d831
Miguel Gomez <miguelgcrespo07@gmail.com>
2019-06-05 19:40:02 -0500

    Pruebas

--- php/productos.php
+++ php/productos.php
@@ -6,15 +6,33 @@
 	<link rel="stylesheet" href="../css/estilo.css">
 </head>
 <body>
-		<?php 
+	<?php 
 		include("../includes/menu.php"); 
-	 ?>
+	?>
 	<section class="ContenedorPrincipal">
+		<form action="productos_busqueda.php" method="GET" class="form_buscador">
+			<input type="text" name="busqueda" placeholder="Glucheck">
+			<button id="btn_busqueda" type="submit" name="buscar"><img src="../img/lupa.png" alt=""></button>
+		</form>
 		<div class="titulopagina">Productos</div>
 		<?php
 				include("conexion.php");
+				$sql_numr = mysqli_query($conexion,"SELECT COUNT(*) as total FROM cat_productos WHERE estado = 'Alta';");
+				$total_registros = mysqli_fetch_array($sql_numr);
+				$totalregistros = $total_registros['total'];
 
-				$sql = "SELECT * FROM cat_productos";
+				$porpagina = 2;
+
+				if (empty($_GET['pagina'])) {
+					$pagina = 1;
+				}else{
+					$pagina = $_GET['pagina'];
+				}
+
+				$desde = ($pagina - 1) * $porpagina;
+				$totalpagina = ceil($totalregistros / $porpagina);
+
+				$sql = "SELECT * FROM cat_productos WHERE estado = 'Alta' LIMIT $desde,$porpagina";
 
 				if(!$resultado = $conexion->query($sql)){
 					die('Ocurrio un error ejecutando el query [' . $conexion->error . ']');
@@ -37,6 +55,39 @@
 					</div>";
 				}				
 			?>
+			<?php if ($totalregistros !=0) { ?>
+		<div class="paginador">
+			<ul>
+				<?php 	
+					if ($pagina != 1) {
+				 ?>
+				<li><a href="?pagina=<?php 	echo 1 ?>">|<</a></li>
+				<li><a href="?pagina=<?php 	echo $pagina-1 ?>"><</a></li>
+				<?php 	
+					}
+				 ?>
+				<?php 	
+					for ($i	=1; $i < $totalpagina+1 ; $i++) { 
+						if ($i == $pagina) 
+							echo "<li class='paginaseleccionada'>".$i."</li>";
+						else
+							echo "<li><a href='?pagina=".$i."'>".$i."</a></li>";
+					}
+
+				 ?>
+
+				<?php 	
+					if ($pagina != $totalpagina) {
+				 ?>
+
+				<li><a href="?pagina=<?php 	echo $pagina+1 ?>">></a></li>
+				<li><a href="?pagina=<?php 	echo $totalpagina ?>">>|</a></li>
+				<?php 	
+					}
+				 ?>
+			</ul>
+		</div>
+	<?php } ?>	
 		
 		
 
--- php/productos_busqueda.php
+++ php/productos_busqueda.php
@@ -20,16 +20,29 @@
 	 ?>
 	<section class="ContenedorPrincipal">
 		<form action="productos_busqueda.php" method="GET" class="form_buscador">
-			<input type="text" name="busqueda" placeholder="">
+			<input type="text" name="busqueda" placeholder="Glucheck">
 			<button id="btn_busqueda" type="submit" name="buscar"><img src="../img/lupa.png" alt=""></button>
 		</form>
-		<br>	<br>	
 		<div class="titulopagina">Productos</div>
 
 		<?php
 				include("conexion.php");
+				$sql_numr = mysqli_query($conexion,"SELECT COUNT(*) as total FROM cat_productos WHERE (NombreProd LIKE '%$busqueda%' OR DescripcionProd LIKE '%$busqueda%' ) AND estado = 'Alta'");
+				$total_registros = mysqli_fetch_array($sql_numr);
+				$totalregistros = $total_registros['total'];
 
-				$sql = "SELECT * FROM cat_productos WHERE NombreProd LIKE '%$busqueda%'";
+				$porpagina = 2;
+
+				if (empty($_GET['pagina'])) {
+					$pagina = 1;
+				}else{
+					$pagina = $_GET['pagina'];
+				}
+
+				$desde = ($pagina - 1) * $porpagina;
+				$totalpagina = ceil($totalregistros / $porpagina);
+
+				$sql = "SELECT * FROM cat_productos WHERE (NombreProd LIKE '%$busqueda%' OR DescripcionProd LIKE '%$busqueda%' ) AND estado = 'Alta'";
 
 				if(!$resultado = $conexion->query($sql)){
 					die('Ocurrio un error ejecutando el query [' . $conexion->error . ']');
@@ -52,6 +65,39 @@
 					</div>";
 				}				
 			?>
+			<?php if ($totalregistros !=0) { ?>
+		<div class="paginador">
+			<ul>
+				<?php 	
+					if ($pagina != 1) {
+				 ?>
+				<li><a href="?pagina=<?php 	echo 1 ?>">|<</a></li>
+				<li><a href="?pagina=<?php 	echo $pagina-1 ?>"><</a></li>
+				<?php 	
+					}
+				 ?>
+				<?php 	
+					for ($i	=1; $i < $totalpagina+1 ; $i++) { 
+						if ($i == $pagina) 
+							echo "<li class='paginaseleccionada'>".$i."</li>";
+						else
+							echo "<li><a href='?pagina=".$i."'>".$i."</a></li>";
+					}
+
+				 ?>
+
+				<?php 	
+					if ($pagina != $totalpagina) {
+				 ?>
+
+				<li><a href="?pagina=<?php 	echo $pagina+1 ?>">></a></li>
+				<li><a href="?pagina=<?php 	echo $totalpagina ?>">>|</a></li>
+				<?php 	
+					}
+				 ?>
+			</ul>
+		</div>
+	<?php } ?>	
 		
 		
 
